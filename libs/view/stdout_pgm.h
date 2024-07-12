a3a68a0fe9136f75eb8bae7616e3d6d1763a2971
Debjeet Banerjee <136410764+mintRaven-05@users.noreply.github.com>
2024-07-12 16:54:19 +0530

    added libs/view/stdout_pgm.h

---
+++ libs/view/stdout_pgm.h
@@ -0,0 +1,12 @@
+#ifndef STDOUT_PGM_H_
+#define STDOUT_PGM_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "../utils/header/header.h"
+#include "../utils/program/PGM_x64.h"
+#include "../decoder/program/decode_PGM_x64.h"
+
+void ViewProgramHeaderInfo_x64(PGM_HEADER_x64 *, int);
+
+#endif
