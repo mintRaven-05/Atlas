cade358730301645e837ab476d95b771ae16ac4b
Debjeet Banerjee <136410764+mintRaven-05@users.noreply.github.com>
2024-08-10 12:45:38 +0530

    [METADATA]

--- libs/view/stdout_help.h
+++ libs/view/stdout_help.h
@@ -3,6 +3,14 @@
 
 #include <stdio.h>
 
+#define _AUTHOR_    "Debjeet Banerjee (mintRaven)"
+#define _DATE_      "19th July, 2024"
+#define _TOOL_      "Atlas"
+#define _LICENSE_   "BSD 3-Clause"
+#define _COPYRIGHT_ "Copyright (C) 2024, Debjeet Banerjee"
+#define _VERSION_   "0.0.2"
+#define _WEBSITE_   "https://github.com/mintRaven-05
+
 void ViewVersionInfo();
 void ViewHelpScreen();
 
